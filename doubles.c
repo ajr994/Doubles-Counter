66dfc386935cbfa2f630a1f7720ddb8675db8d19
Alexander Rich <47109139+ajr994@users.noreply.github.com>
2019-05-07 20:42:26 +0100

    Update doubles.c

--- doubles.c
+++ doubles.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "numberfinal.txt"
+#include "numberinformation.h"
 
 double *data = NULL;            //Pointer to the data, initialized at NULL
 int data_allocated = 0;         //Data allocated at start, will change later
@@ -14,7 +14,7 @@ int readfile(const char *filename)  {
     FILE *f = fopen(filename, "r");
     if (f == NULL)  {
         fprintf(stderr, "Can't open %s\n", filename);
-        return 1;           //fails to open file
+        return 1;                           //Fails to open file
     }
     double datum;
     int status;
@@ -30,31 +30,31 @@ int readfile(const char *filename)  {
             data = realloc(data, data_allocated * sizeof(data[0]));
             if (data == 0)   {
                 fprintf(stderr, "reallocate to %d failed\n", data_allocated);
-                return 2;           //fails
+                return 2;                    //Fails to reallocate more space
             }
         }
-        data[data_count++] = datum;
+        data[data_count++] = datum;          //Increment data_count and use as adress of data array, store value in datum
     }
     fclose(f);
-    sorted = false;
+    sorted = false;                          //Have the data now, but isn't sorted so change to false
     
     if (status == 0)    {
         fprintf(stderr, "Non-number found in %s\n", filename);
-        return 1;       //fails
+        return 1;                           //Fails because file is of bad type (not numbers, etc)
     }
-    return 0;           //success
+    return 0;                               //Success 
 }
-int main()  {
+int main()  {                               //This was just included so I could compile on x86
 };
 
-double total()  {
+double total()  {                           //Initialize sum as a double at 0.0, for every data_count adrress take the value, add to sum and store back in sum
     double sum = 0.0;
     for (int i=0; i<data_count; i++)
         sum += data[i];
     return sum;
 }
 
-// Compare two doubles for qsort
+                                            // Compare two doubles for qsort
 int compare_doubles(const void *va, const void *vb) {
     const double *a = va, *b = vb;
     if (*a < *b) return -1;
@@ -72,14 +72,14 @@ double value(int index) {
     else
         return data[index];
 }
-// How much data was read?
+                                           // How much data was read?
 int count() {
     return data_count;
 }
-//This is called before anything happens, and is defined in the .h file
+                                           //This is called before anything happens, and is defined in the header file
 void initialize()   {
 }
-//done, free the memory now
+                                           //done, free the memory now
 void terminate()    {
-    free(data);     //gives up the memory
+    free(data);                            //gives up allocated memory
 }
